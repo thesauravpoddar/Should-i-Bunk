ed3e43aa1f911f908c2880eb5de0d363224fad70
thesauravpoddar <thesauravpoddar@gmail.com>
2025-07-17 14:24:46 +0530

    Initial Commit

---
+++ src/main/java/com/should_i_bunk/should_i_bunk/ShouldIBunkApplication.java
@@ -0,0 +1,13 @@
+package com.should_i_bunk.should_i_bunk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShouldIBunkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShouldIBunkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/should_i_bunk/should_i_bunk/ShouldIBunkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.should_i_bunk.should_i_bunk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShouldIBunkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
